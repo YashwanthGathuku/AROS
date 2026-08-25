62b879b52018a32a0817f24ede9753fe3d6bf77e
Sphinxghost <90673620+YashwanthGathuku@users.noreply.github.com>
2026-08-25 17:28:49 -0400

    export IntentResult decode helpers from ipc package

--- python/aros_research/ipc/__init__.py
+++ python/aros_research/ipc/__init__.py
@@ -1,10 +1,12 @@
 from .framing import IpcError, decode_header, encode_frame
-from .wire import Hello, encode_hello, encode_tool_intent
+from .wire import Hello, IntentResult, decode_intent_result, encode_hello, encode_tool_intent
 
 __all__ = [
     "Hello",
+    "IntentResult",
     "IpcError",
     "decode_header",
+    "decode_intent_result",
     "encode_frame",
     "encode_hello",
     "encode_tool_intent",
